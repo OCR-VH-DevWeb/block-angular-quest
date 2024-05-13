eba7c558d55ebbbe1ac0491df4cb0719a93dc11d
Vivi Valienne <valiendanse@gmail.com>
2024-05-13 18:17:54 +0200

    add challenge 02 Binding / Create a user-profile-component / Start of css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BlockComponent } from './block/block.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BlockComponent],
+  imports: [RouterOutlet, BlockComponent, UserProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface User {
+  name: string;
+  firstName: string;
+  age: number;
+  quote: string;
+  photo: string;
+}
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css',
+})
+export class UserProfileComponent {
+  user: User = {
+    name: 'Doe',
+    firstName: 'John',
+    age: 25,
+    quote: '',
+    photo: 'https://randomuser.me/api/portraits/lego/2.jpg',
+  };
+
+  hideAge: boolean = false;
+}
